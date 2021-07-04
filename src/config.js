d21c1448123298fa3847648cf5a172dcfd93b02a
Austin Chamney <achamney@gmail.com>
2021-07-03 21:10:18 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   SIZE_LIMIT: 50, // mentioned in KB
   PORT: process.env.PORT || 3000,
-  MONGO_URL: process.env.MONGODB_URI || "mongodb://localhost:27017/jsonbox-io-dev",
+  MONGO_URL: atob("bW9uZ29kYitzcnY6Ly9qc29uYm94eTpzZWdjb3Vyc2U+QGNsdXN0ZXIwLjdwNzhlLm1vbmdvZGIubmV0L215Rmlyc3REYXRhYmFzZT9yZXRyeVdyaXRlcz10cnVlJnc9bWFqb3JpdHk=") || "mongodb://localhost:27017/jsonbox-io-dev",
 };
